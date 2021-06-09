4962321772bd313d10765812aa49fce87ef87209
gizemgizg <lyl.mlck0693@gmail.com>
2021-06-09 22:27:38 +0300

    first commit

---
+++ dikdortgenalan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dikdortgenalan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kisa, uzun, alan, cevre;
+
+            Console.Write("Kısa Kenar : ");
+            kisa = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Uzun Kenar : ");
+            uzun = Convert.ToInt32(Console.ReadLine());
+
+            alan = kisa * uzun;
+
+            cevre = 2 * (kisa + uzun);
+
+            Console.WriteLine("Alan :{0} ", alan);
+            Console.WriteLine("Çevre :{0} " , cevre);
+            Console.ReadKey();
+        }
+    }
+}
